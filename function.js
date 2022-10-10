e127c74aa370d7f5f73ca6ed3abec8b78568b851
kaemoney0147 <114649936+kaemoney0147@users.noreply.github.com>
2022-10-10 15:57:42 +0100

    code in progress

---
+++ function.js
@@ -0,0 +1,8 @@
+const h1 = document.querySelector('h1')
+h1.innerText = 'Auto Mobile';
+document.body.style.backgroundColor = "coral";
+const footer = document.querySelector('footer');
+footer.innerText = '12443 shaw road usa 92e2';
+
+
+
