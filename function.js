80ccb5a780baff2e219a27d759184f8ce240a188
kaemoney0147 <114649936+kaemoney0147@users.noreply.github.com>
2022-10-12 09:01:37 +0100

    Code in progress done

--- function.js
+++ function.js
@@ -1,8 +1,36 @@
-const h1 = document.querySelector('h1')
-h1.innerText = 'Auto Mobile';
-document.body.style.backgroundColor = "coral";
-const footer = document.querySelector('footer');
-footer.innerText = '12443 shaw road usa 92e2';
+function changeH1text() {
+    let newText = document.querySelector('h1');
+    newText.innerText = 'Dynamic Laptop Store ';
 
+}
+function bodyColor() {
+    document.body.style.backgroundColor = 'coral';
+}
+function changeAddress(change) {
+    let addressChaneg = document.querySelector('footer>p');
+    addressChaneg.innerHTML = change;
+}
+function addClassToAmazonLink() {
+    for (let node of document.querySelectorAll("a")) {
+        node.classList.add("amazon");
+    }
+}
+function toggleImages() {
+    for (let image of document.querySelectorAll("img")) {
+        image.classList.toggle("hidepics");
+    }
+}
+function setRandomColor() {
+    const color = getRandomColor();
+    for (let price of document.querySelectorAll("tr > td:nth-child(4)")) {
+        price.style.color = color;
+    }
+}
 
+function getRandomColor() {
+    const red = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
 
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
